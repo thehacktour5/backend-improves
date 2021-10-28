611db038358ebeea693206f1f9c40353baba3f16
Fedorov <atiliogoulart@gmail.com>
2021-10-27 22:10:28 -0300

    Update

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+api = Api(app)
+
+alunos = {
+       "Atilio":{
+              "idade":25,
+              "gostosura":"lindo"
+       },
+       "Hector":{
+              "idade":25,
+              "gostosura":"lindo"
+       }
+}
+
+class AlunosEndPoint(Resource):
+       def get(self):
+              return [alunos[key] for key in alunos.keys()]
+
+class AlunosGeralEndPoint(Resource):
+       def put(self, alunos_id):
+              alunos[alunos_id] = request.form['data']
+              return {alunos: alunos[alunos_id]}
+
+api.add_resource(AlunosEndPoint, '/')
+api.add_resource(AlunosGeralEndPoint, '/alunos/<string:alunos>/')
+
+if __name__ == "__main__":
+       app.run()
